ecfb1b4f935d579af2a7b208ab0cd8736fc6e314
khanarham0029 <khanarham0029@gmail.com>
2025-07-15 20:27:43 +0500

    removed sentry

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -1,16 +1,16 @@
-// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
-// The config you add here will be used whenever one of the edge features is loaded.
-// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+// // This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// // The config you add here will be used whenever one of the edge features is loaded.
+// // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+// import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://4c83845fda0cfb6ead7af2649952d70b@o4509673208741888.ingest.de.sentry.io/4509673238036560",
+// Sentry.init({
+//   dsn: "https://4c83845fda0cfb6ead7af2649952d70b@o4509673208741888.ingest.de.sentry.io/4509673238036560",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+//   // Adjust this value in production, or use tracesSampler for greater control
+//   tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+//   // Setting this option to true will print useful information to the console while you're setting up Sentry.
+//   debug: false,
+// });
